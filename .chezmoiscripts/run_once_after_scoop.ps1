6a87495eae4803989b59c6bb83a1062982ae96ad
Connor Edwards <38229097+cedws@users.noreply.github.com>
2025-04-23 21:59:00 +0900

    Add croc

--- .chezmoiscripts/run_once_after_scoop.ps1
+++ .chezmoiscripts/run_once_after_scoop.ps1
@@ -1,4 +1,4 @@
 iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 scoop install git
 scoop bucket add extras
-scoop install extras/windowsdesktop-runtime@8.0.15 tinynvidiaupdatechecker
+scoop install windowsdesktop-runtime@8.0.15 tinynvidiaupdatechecker croc
