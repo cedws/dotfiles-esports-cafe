327ef54621a2467629f3bcbcbd104fef8f227165
Connor Edwards <38229097+cedws@users.noreply.github.com>
2025-04-23 22:46:38 +0900

    Run TinyNvidiaUpdateChecker on init

--- .chezmoiscripts/run_once_after_scoop.ps1
+++ .chezmoiscripts/run_once_after_scoop.ps1
@@ -2,3 +2,4 @@ iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 scoop install git
 scoop bucket add extras
 scoop install windowsdesktop-runtime@8.0.15 tinynvidiaupdatechecker croc
+tinynvidiaupdatechecker.exe --quiet --noprompt --confirm-dl
