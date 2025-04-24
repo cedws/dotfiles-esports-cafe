4c061eb75b4869ff7791aca46f56316b63ab9fc0
Connor Edwards <38229097+cedws@users.noreply.github.com>
2025-04-24 23:38:08 +0900

    Add 7zip

--- .chezmoiscripts/run_once_after_001_scoop.ps1
+++ .chezmoiscripts/run_once_after_001_scoop.ps1
@@ -2,4 +2,4 @@ iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 
 scoop install git
 scoop bucket add extras
-scoop install windowsdesktop-runtime@8.0.15 tinynvidiaupdatechecker croc
+scoop install windowsdesktop-runtime@8.0.15 tinynvidiaupdatechecker croc 7zip
